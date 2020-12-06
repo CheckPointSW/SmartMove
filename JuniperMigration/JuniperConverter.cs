91ea33ff4d77a7f5a502124c94336a4d4a5f6162
Nastassia.Arefyeva@ibm.com <Nastassia.Arefyeva@ibm.com>
2020-12-06 19:49:31 +0300

    Generation of SmartConnector related content for each vendor is added

--- JuniperMigration/JuniperConverter.cs
+++ JuniperMigration/JuniperConverter.cs
@@ -3560,6 +3560,8 @@ namespace JuniperMigration
             // Resolve the conversion categories/lines count to report to the user.
             ConversionIncidentCategoriesCount = _conversionIncidents.GroupBy(error => error.Title).Count();
             ConversionIncidentsCommandsCount = _conversionIncidents.GroupBy(error => error.LineNumber).Count();
+			
+			CreateSmartConnector();
         }
         
         public override int RulesInConvertedPackage()
