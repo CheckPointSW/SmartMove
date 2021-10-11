81726f2d787742725ff8de63edd365982a817b42
simonmeadows <9550767+simonmeadows@users.noreply.github.com>
2021-10-11 09:28:04 +0100

    Network Object mapping fix
    Changed indent of break to re-instate inaccessible code block for mapping existing network objects

--- SmartMove/SmartConnector/smartconnector.py
+++ SmartMove/SmartConnector/smartconnector.py
@@ -194,6 +194,15 @@ def addCpObjectWithIpToServer(client, payload, userObjectType, userObjectIp, mer
                                 printStatus(None, "REPORT: " + "CP object " + mergedObjectsNamesMap[
                                     userObjectNameInitial] + " is used instead of " + userObjectNameInitial)
                                 isFinished = True
+                                break
+                            for serverObject in res_get_obj_with_ip.data:
+                                mergedObjectsNamesMap[userObjectNameInitial] = serverObject['name']
+                                if (isServerObjectLocal(serverObject) and not isReplaceFromGlobalFirst) or (
+                                        isServerObjectGlobal(serverObject) and isReplaceFromGlobalFirst):
+                                    break
+                            printStatus(None, "REPORT: " + "CP object " + mergedObjectsNamesMap[
+                                userObjectNameInitial] + " is used instead of " + userObjectNameInitial)
+                            isFinished = True
                             break
                             
                     else:
