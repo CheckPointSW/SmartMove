ac1ba0b815c3f7af4435ce64592eaa77c27a6a57
Shibalov Roman <shibalov.roman@gmail.com>
2021-11-16 13:02:53 +0300

    add creation of cp_objects_opt.json for FortiNet vendor

--- FortinetMigration/FortiGateConverter.cs
+++ FortinetMigration/FortiGateConverter.cs
@@ -1303,7 +1303,8 @@ namespace FortiGateMigration
             _warningsConvertedPackage += _warningsList.Count;
             _errorsConvertedPackage += _errorsList.Count;
 
-            CreateSmartConnector();
+            CreateSmartConnector(true, false);      //cp_objects.json
+            CreateSmartConnector(true, true);       //cp_objects_opt.json
 
             // to clean; must be the last!!!
             _cpObjects.ClearRepository();
