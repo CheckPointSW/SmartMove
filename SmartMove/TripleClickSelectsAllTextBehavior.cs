19cf06ffa59bfa102b506851930f3f884dde8ab1
ASemenchuk <59782501+Botinoc@users.noreply.github.com>
2022-07-13 12:08:51 +0300

    fix issue with nugets, update System.Runtime.Serialization.Json to Newtonsoft.Json

--- SmartMove/TripleClickSelectsAllTextBehavior.cs
+++ SmartMove/TripleClickSelectsAllTextBehavior.cs
@@ -15,8 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ********************************************************************/
 
+using Microsoft.Xaml.Behaviors;
 using System.Windows.Controls;
-using System.Windows.Interactivity;
 
 namespace SmartMove
 {
